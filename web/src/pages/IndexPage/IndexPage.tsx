4772b53e99d7ac7e9439ef0b248c6fd51bc0ddbb
Yuri Parsons <yuri@ninjakitty.net>
2023-10-22 18:34:22 +0100

    clean up mess

--- web/src/pages/IndexPage/IndexPage.tsx
+++ web/src/pages/IndexPage/IndexPage.tsx
@@ -1,47 +1,11 @@
 import { useState } from 'react'
 
-import { Text } from '@mantine/core'
+import { Center, Text, Title } from '@mantine/core'
 
 import Header from 'src/components/Header/Header'
 
-import { ColorSchemeToggle } from '../../components/ColorSchemeToggle/ColorSchemeToggle'
 import { Welcome } from '../../components/Welcome/Welcome'
-/*
-drag queens (food):  Jan, Pearl, Rosé, Jade
-
-animals: Fox, bunny, cat, dog
-
-Meagans:  Good, Garcia-McLain, Fox, Stallion
-
-Food: Peppermint, Ginger, Chamomile, Sage
-*/
-
-const OGdata = [
-  // queens
-  { name: 'Jan', id: 'jan', found: false, group: 'queens' },
-  { name: 'Pearl', id: 'pearl', found: false, group: 'queens' },
-  { name: 'Rosé', id: 'rose', found: false, group: 'queens' },
-  { name: 'Jade', id: 'jade', found: false, group: 'queens' },
-
-  // pets
-  { name: 'Fox', id: 'fox', found: false, group: 'pets' },
-  { name: 'Bunny', id: 'bunny', found: false, group: 'pets' },
-  { name: 'Cat', id: 'cat', found: false, group: 'pets' },
-  { name: 'Dog', id: 'dog', found: false, group: 'pets' },
-  // meagans
-  { name: 'Good', id: 'good', found: false, group: 'Megs' },
-  { name: 'Garcia-McLain', id: 'garcia-mclain', found: false, group: 'Megs' },
-  { name: 'Markle', id: 'markle', found: false, group: 'Megs' },
-  { name: 'Stallion', id: 'stallion', found: false, group: 'Megs' },
-  // food
-  { name: 'Peppermint', id: 'peppermint', found: false, group: 'food' },
-  { name: 'Ginger', id: 'ginger', found: false, group: 'food' },
-  { name: 'Chamomile', id: 'chamomile', found: false, group: 'food' },
-  { name: 'Sage', id: 'sage', found: false, group: 'food' },
-]
-
 export default function IndexPage() {
-  // const [data, setData] = useState(OGdata.sort(() => Math.random() - 0.5))
   const [list, setList] = useState(
     [
       // queens
@@ -57,8 +21,8 @@ export default function IndexPage() {
       // meagans
       { name: 'Good', id: 'good', found: false, group: 'Megs' },
       {
-        name: 'Garcia-McLain',
-        id: 'garcia-mclain',
+        name: 'Boone',
+        id: 'boone',
         found: false,
         group: 'Megs',
       },
@@ -69,7 +33,7 @@ export default function IndexPage() {
       { name: 'Ginger', id: 'ginger', found: false, group: 'food' },
       { name: 'Chamomile', id: 'chamomile', found: false, group: 'food' },
       { name: 'Sage', id: 'sage', found: false, group: 'food' },
-    ].sort(() => Math.random() - 0.5)
+    ] //.sort(() => Math.random() - 0.5)
   )
 
   // guessed groups
@@ -101,6 +65,9 @@ export default function IndexPage() {
 
   return (
     <>
+      <Center style={{ margin: 32 }}>
+        <Title order={3}>Make connections between words</Title>
+      </Center>
       <Header groups={groups} />
       {count < 4 && <Welcome list={list} onSubmit={handleSubmit} />}
       {count === 4 && <Text color="red">Try again next time!</Text>}
